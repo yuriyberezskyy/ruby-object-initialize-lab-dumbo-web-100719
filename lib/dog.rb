578f17c2db08a35d03933b3aa730bb62a78b27f3
Yuriy Berezskyy <bereza95@gmail.com>
2019-10-08 20:42:07 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def initialize(name,breed="Mutt")
+      @name = name
+      @breed = breed
+  end
+  
+  
+  def name 
+    @name
+  end
+  
+  def name=(newName)
+    @name = newName
+  end
+  
+  def breed
+    @breed 
+  end
+  
+  def breed=(newBreed)
+    @breed = newBreed
+  end
+  
+end
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  def initialize(name)
+    @name = name 
+  end
+  
+  def name 
+    @name 
+  end
+  
+  def name=(newName)
+    @name = newName
+  end
+end
